4d85f8a99155cc0bc3401960192de1ec28c6e731
jrcayetano <josercayetano@gmail.com>
2018-10-07 11:34:59 +0200

    Added pagination

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -3,7 +3,7 @@ import PropsTypes from 'prop-types';
 import _ from 'lodash';
 const Pagination = props => {
   const { itemsCount, pageSize, onPageChange, currentPage } = props;
-  console.log(currentPage);
+
   const pagesCount = Math.ceil(itemsCount / pageSize);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
