fed2cf3329a0fdab64d08210c052da107346fb9d
jrcayetano <josercayetano@gmail.com>
2018-10-07 11:10:43 +0200

    Added pagination

--- src/components/TableMovie.jsx
+++ src/components/TableMovie.jsx
@@ -1,63 +1,89 @@
 import React, { Component } from 'react';
-import  { getMovies }  from '../services/fakeMovieService';
-import Movie from './Movie'
+import { getMovies } from '../services/fakeMovieService';
+import Movie from './Movie';
+import Pagination from './common/Pagination';
+import { paginate } from '../utils/paginate';
 
 class TableMovie extends Component {
-  
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       movies: [],
-      headerTable: ['Title', 'Genre', 'Stock', 'Rate', 'liked', '']
-    }
+      headerTable: ['Title', 'Genre', 'Stock', 'Rate', 'liked', ''],
+      pageSize: 4,
+      currentPage: 1
+    };
   }
 
   componentDidMount() {
-    this.setState({movies: getMovies()})
+    this.setState({ movies: getMovies() });
   }
 
   deletMovieHandle = movie => {
-    console.log('delete', movie)
+    console.log('delete', movie);
     const movieList = [...this.state.movies];
     const movieListClear = movieList.filter(movieItem => movieItem !== movie);
-    this.setState({movies: movieListClear})
-  }
+    this.setState({ movies: movieListClear });
+  };
   handleLike = movie => {
     let movieList = [...this.state.movies];
     movieList = movieList.map(movieItem => {
-      if (movie === movieItem) movieItem['liked'] = !movieItem['liked']
+      if (movie === movieItem) movieItem['liked'] = !movieItem['liked'];
       return movieItem;
-    })
-    this.setState({movies: movieList}) 
-    
-  }
+    });
+    this.setState({ movies: movieList });
+  };
 
-  render() { 
-    const movieMessage = this.state.movies.length !== 0 ? 
-                          <p>Showing {this.state.movies.length} movies in the database</p> : 
-                          <p>There is not movie on database</p>
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+    console.log('Page change', page);
+  };
+
+  render() {
+    const { currentPage, pageSize, movies: allMovies } = this.state;
+
+    const movieMessage =
+      this.state.movies.length !== 0 ? (
+        <p>Showing {this.state.movies.length} movies in the database</p>
+      ) : (
+        <p>There is not movie on database</p>
+      );
+    const movies = paginate(allMovies, currentPage, pageSize);
+
     return (
       <React.Fragment>
         {movieMessage}
         <table className="table">
           <thead>
             <tr>
-              {this.state.headerTable.map(header => <th scope="col" key={header}> {header} </th>)}
+              {this.state.headerTable.map(header => (
+                <th scope="col" key={header}>
+                  {' '}
+                  {header}{' '}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => 
-              <Movie 
-              movie={movie} 
-              onDelete={ () => this.deletMovieHandle(movie)} 
-              key={movie._id} 
-              onLike={() => this.handleLike(movie)}/>)
-            }
+            {movies.map(movie => (
+              <Movie
+                movie={movie}
+                onDelete={() => this.deletMovieHandle(movie)}
+                key={movie._id}
+                onLike={() => this.handleLike(movie)}
+              />
+            ))}
           </tbody>
         </table>
+        <Pagination
+          itemsCount={this.state.movies.length}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
 }
- 
+
 export default TableMovie;
---
+++ src/components/common/Pagination.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropsTypes from 'prop-types';
+import _ from 'lodash';
+const Pagination = props => {
+  const { itemsCount, pageSize, onPageChange, currentPage } = props;
+  console.log(currentPage);
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        {pages.map(page => (
+          <li
+            className={page === currentPage ? 'page-item active' : 'page-item'}
+            key={page}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+Pagination.propsTypes = {
+  itemsCount: PropsTypes.number.isRequired,
+  pageSize: PropsTypes.number.isRequired,
+  onPageChange: PropsTypes.func.isRequired,
+  currentPage: PropsTypes.number.isRequired
+};
+export default Pagination;
